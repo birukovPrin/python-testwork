d8395be3c3d998f2ba97e0084c0998795224ccea
admin <x123dws@gmail.com>
2016-08-13 15:20:52 +0300

    val

--- loginsys/models.py
+++ loginsys/models.py
@@ -2,12 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core import validators
+from django.core.validators import RegexValidator
 
 
 class Profile(models.Model):
+
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
-    phone = models.CharField(max_length=11, blank=True, null=True)
-    skype = models.CharField(max_length=30, blank=True, null=True)
+
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,11}$', message="Phone number must be entered in the format: '+999999999'")
+    phone = models.CharField(max_length=11, validators=[phone_regex], blank=True, null=True)
+
+    skype = models.CharField(validators=[validators.validate_slug], max_length=30, blank=True, null=True)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
--- testblog/models.py
+++ testblog/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-
+from django.core import validators
 
 class Post(models.Model):
 
     username = models.CharField(max_length=50,default="guest")
-    title = models.CharField(max_length=50)
+    title = models.CharField(validators=[validators.validate_slug], max_length=50)
     text = models.TextField()
     date = models.DateTimeField(
             default=timezone.now)
